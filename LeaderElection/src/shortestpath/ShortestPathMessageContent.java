a754acc9cd2138efc7c517ae887baf2436073cea
Ramya Rangan <ramyarangan@college.harvard.edu>
2016-05-08 00:36:37 -0400

    started pair

--- LeaderElection/src/shortestpath/ShortestPathMessageContent.java
+++ LeaderElection/src/shortestpath/ShortestPathMessageContent.java
@@ -1,7 +1,11 @@
 package shortestpath;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import util.Pair;
 import common.MessageContent;
 
 public class ShortestPathMessageContent extends MessageContent {
-
+	HashMap<Pair, ArrayList<Integer>> paths;
 }

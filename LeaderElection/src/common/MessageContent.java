6840fe7e10f961ed2114e5240f40b3827ed43a38
Kevin Wang <kwang01@college.harvard.edu>
2016-05-08 17:29:32 -0400

    change some method visibilities, add some comments, and reformat

--- LeaderElection/src/common/MessageContent.java
+++ LeaderElection/src/common/MessageContent.java
@@ -1,16 +1,43 @@
 package common;
 
+import baseline.BaselineProcess;
+
+/**
+ * This represents the content of a message. It is overridden by each separate
+ * simulation to include custom contents. For example, @see
+ * {@link BaselineProcess}
+ * 
+ * @author kwang01
+ *
+ */
 public class MessageContent {
+	/**
+	 * Message body
+	 */
 	private String body;
-	
+
+	/**
+	 * Simple constructor without body, setting it to null
+	 */
 	public MessageContent() {
 		body = null;
 	}
-	
+
+	/**
+	 * Simple constructor
+	 * 
+	 * @param body
+	 *            the desired body
+	 */
 	public MessageContent(String body) {
 		this.body = body;
 	}
-	
+
+	/**
+	 * Simple getter
+	 * 
+	 * @return body
+	 */
 	public String getBody() {
 		return body;
 	}
